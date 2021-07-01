f247883fbf21f73ce93fd58623e8183f3a992cf8
Skyler Edvik <edvik.skyler@gmail.com>
2021-07-01 16:39:06 -0600

    modify server.js to not always re-seed db after starting server. seedDbs.js created to perform this task

---
+++ seedDbs.js
@@ -0,0 +1,98 @@
+const parse = require('csv-parse');
+const sqlite3 = require('sqlite3');
+const fs = require('fs');
+const {createCountryTable, createGoldMedalTable} = require('./sql');
+
+const db = new sqlite3.Database('./gold_medals.sqlite');
+console.log('Seeding database. This may take several minutes...');
+
+db.serialize(() => {
+    // Drop the tables if they exist so that they can be re-seeded
+    db.run("DROP TABLE IF EXISTS Country;");
+    db.run("DROP TABLE IF EXISTS GoldMedal;");
+
+    const createCountryQuery = createCountryTable();
+    const createGoldMedalQuery = createGoldMedalTable();
+
+    // Create the country table
+    if (createCountryQuery) {
+      db.run(createCountryQuery, err => {
+        if (err) {
+          console.log("Error while creating the Country table!");
+          console.log(err);
+          return;
+        }
+      });
+    }
+
+    // Create the GoldMedal table
+    if (createGoldMedalQuery) {
+      db.run(createGoldMedalQuery, err => {
+        if (err) {
+          console.log("Error while creating the GoldMedal table!");
+          console.log(err);
+          return;
+        }
+      });
+    }
+
+    // Add the Country data
+    db.run("DELETE FROM Country;", (err) => {
+      if (err) {
+        console.log("Country table not found!");
+        console.log(err);
+        return;
+      } else {
+        fs.createReadStream('data/country.csv')
+        .pipe(parse({from: 2}))
+        .on('data', function(csvrow) {
+          db.run("INSERT INTO Country (name, code, population, gdp) VALUES ($name, $code, $population, $gdp)", {
+            $name: csvrow[0],
+            $code: csvrow[1],
+            $population: csvrow[2],
+            $gdp: csvrow[3]
+          }, (err) => {
+            if (err) {
+              console.log("Error while inserting Country data");
+              console.log(err);
+              return;
+            }
+          });
+        });
+      }
+    });
+
+    // Add the GoldMedal data
+    db.run("DELETE FROM GoldMedal;", (err) => {
+      if (err) {
+        console.log("GoldMedal table not found!");
+        console.log(err);
+        return;
+      } else {
+        let idCounter = 1;
+        fs.createReadStream('data/goldmedal.csv')
+        .pipe(parse({from: 2}))
+        .on('data', function(csvrow) {
+          db.run("INSERT INTO GoldMedal (id, year, city, sport, discipline, name, country, gender, event, season) VALUES ($id, $year, $city, $sport, $discipline, $name, $country, $gender, $event, $season)", {
+            $id: idCounter,
+            $year: csvrow[0],
+            $city: csvrow[1],
+            $sport: csvrow[2],
+            $discipline: csvrow[3],
+            $name: csvrow[4],
+            $country: csvrow[5],
+            $gender: csvrow[6],
+            $event: csvrow[7],
+            $season: csvrow[8]
+          }, (err) => {
+            if (err) {
+              console.log("Error while inserting GoldMedal data");
+              console.log(err);
+              return;
+            }
+          });
+          idCounter += 1;
+        });
+      }
+    });
+});
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const parse = require('csv-parse');
 const sqlite3 = require('sqlite3');
 const cors = require('cors');
 const fs = require('fs');
-const {createCountryTable, createGoldMedalTable, goldMedalNumber, bestYear, mostSummerWins, mostWinterWins, bestDiscipline, bestSport, bestEvent, numberMenMedalists, numberWomenMedalists, mostMedaledAthlete, orderedMedals, orderedSports} = require('./sql');
+const {goldMedalNumber, bestYear, mostSummerWins, mostWinterWins, bestDiscipline, bestSport, bestEvent, numberMenMedalists, numberWomenMedalists, mostMedaledAthlete, orderedMedals, orderedSports} = require('./sql');
 
 const db = new sqlite3.Database('./gold_medals.sqlite');
 
@@ -30,7 +30,6 @@ const fixCountryName = countryName => {
 
 app.get('/country/:countryName', (req, res, next) => {
   const countryName = fixCountryName(req.params.countryName);
-
   const goldMedalQuery = goldMedalNumber(countryName);
   const summerWinsQuery = mostSummerWins(countryName);
   const winterWinsQuery = mostWinterWins(countryName);
@@ -308,96 +307,7 @@ app.get('/country/:countryName/sports', (req, res, next) => {
 });
 
 app.listen(3001, () => {
-  let errorThrown = false;
   db.serialize(() => {
-    // Drop the tables if they exist
-    db.run("DROP TABLE IF EXISTS Country;");
-    db.run("DROP TABLE IF EXISTS GoldMedal;");
-
-    const createCountryQuery = createCountryTable();
-    const createGoldMedalQuery = createGoldMedalTable();
-
-    // Create the country table
-    if (createCountryQuery) {
-      db.run(createCountryQuery, err => {
-        if (err) {
-          console.log("Error while creating the Country table!");
-          console.log(err);
-          return;
-        }
-      });
-    }
-
-    // Create the GoldMedal table
-    if (createGoldMedalQuery) {
-      db.run(createGoldMedalQuery, err => {
-        if (err) {
-          console.log("Error while creating the GoldMedal table!");
-          console.log(err);
-          return;
-        }
-      });
-    }
-
-    // Add the Country data
-    db.run("DELETE FROM Country;", (err) => {
-      if (err) {
-        console.log("Country table not found!");
-        console.log(err);
-        return;
-      } else {
-        fs.createReadStream('data/country.csv')
-        .pipe(parse({from: 2}))
-        .on('data', function(csvrow) {
-          db.run("INSERT INTO Country (name, code, population, gdp) VALUES ($name, $code, $population, $gdp)", {
-            $name: csvrow[0],
-            $code: csvrow[1],
-            $population: csvrow[2],
-            $gdp: csvrow[3]
-          }, (err) => {
-            if (err) {
-              console.log("Error while inserting Country data");
-              console.log(err);
-              return;
-            }
-          });
-        });
-      }
-    });
-
-    // Add the GoldMedal data
-    db.run("DELETE FROM GoldMedal;", (err) => {
-      if (err) {
-        console.log("GoldMedal table not found!");
-        console.log(err);
-        return;
-      } else {
-        let idCounter = 1;
-        fs.createReadStream('data/goldmedal.csv')
-        .pipe(parse({from: 2}))
-        .on('data', function(csvrow) {
-          db.run("INSERT INTO GoldMedal (id, year, city, sport, discipline, name, country, gender, event, season) VALUES ($id, $year, $city, $sport, $discipline, $name, $country, $gender, $event, $season)", {
-            $id: idCounter,
-            $year: csvrow[0],
-            $city: csvrow[1],
-            $sport: csvrow[2],
-            $discipline: csvrow[3],
-            $name: csvrow[4],
-            $country: csvrow[5],
-            $gender: csvrow[6],
-            $event: csvrow[7],
-            $season: csvrow[8]
-          }, (err) => {
-            if (err) {
-              console.log("Error while inserting GoldMedal data");
-              console.log(err);
-              return;
-            }
-          });
-          idCounter += 1;
-        });
-      }
-    });
     db.get("SELECT name FROM GoldMedal LIMIT 1;", (err, row) => {
       console.log("Data successfully loaded");
     });
